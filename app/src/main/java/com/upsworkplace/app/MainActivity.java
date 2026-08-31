b64a99237dd6cb3eb1ca9cd894557ae11f7d4cc5
emidiospoto89 <emidiospoto89@hotmail.it>
2026-08-31 02:07:20 +0200

    Update MainActivity.java

--- app/src/main/java/com/upsworkplace/app/MainActivity.java
+++ app/src/main/java/com/upsworkplace/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.upsworkplace.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.graphics.Color;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,13 +13,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getWindow().setStatusBarColor(Color.rgb(53, 28, 21));
+        getWindow().setNavigationBarColor(Color.rgb(53, 28, 21));
+
         WebView web = new WebView(this);
+
+        web.setBackgroundColor(Color.WHITE);
+        web.setFitsSystemWindows(true);
 
         web.setWebViewClient(new WebViewClient());
 
         WebSettings settings = web.getSettings();
+
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
 
         setContentView(web);
 
