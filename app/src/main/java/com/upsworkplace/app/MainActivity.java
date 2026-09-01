2625a29e18fc2bdab745c4e982a8d9dc8af74c49
emidiospoto89 <emidiospoto89@hotmail.it>
2026-09-01 22:14:24 +0200

    Add PdfDocument import to MainActivity

--- app/src/main/java/com/upsworkplace/app/MainActivity.java
+++ app/src/main/java/com/upsworkplace/app/MainActivity.java
@@ -13,6 +13,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
